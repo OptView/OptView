cfe79fa2e6f5ba6bc0dc524ef66943015b2e33f5
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-10-26 20:06:27 -0400

    Create Read/Write API functions for ThingSpeak integration

---
+++ Window Machine Learning/read_arduino_api.py
@@ -0,0 +1,33 @@
+# Read the Arduino Data API
+import requests
+
+base_url = "https://api.thingspeak.com/channels/2316311/feeds.json"
+api_key = ""
+params = {
+    "api_key": api_key,
+    "results": 1
+}
+
+response = requests.get(base_url, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+
+    # Getting the last entry if the "results" is more than 1
+    last_entry = data['feeds'][-1]
+
+    # Extracting field names
+    field_names = {f"field{i}": data['channel'][f'field{i}'] for i in range(1, 8)}
+    print("Field Names:")
+    print(field_names)
+
+    # Extracting field values of the last entry
+    print("\nField Values for the last entry:")
+    field_values = {f"field{i}": last_entry[f'field{i}'] for i in range(1, 8)}
+    print(field_values)
+
+    # Printing the entry_id of the last entry
+    print("\nEntry ID of the last entry:")
+    print(last_entry['entry_id'])
+else:
+    print(f"Failed to retrieve data: {response.status_code}")
---
+++ Window Machine Learning/write_arduino_api.py
@@ -0,0 +1,65 @@
+# Write on API based on the reading of the Arduino Data API
+
+import requests
+import json
+
+# ThingSpeak API settings for reading
+READ_API_BASE_URL = "https://api.thingspeak.com/channels/2316311/feeds.json"
+READ_API_KEY = ""
+
+# ThingSpeak API settings for writing
+WRITE_API_BASE_URL = "https://api.thingspeak.com/update"
+WRITE_API_KEY = ""
+
+# Testing threshold for turning On/Off
+TEMPERATURE_THRESHOLD = 20  # Example threshold
+
+def get_last_temperature():
+    params = {
+        "api_key": READ_API_KEY,
+        "results": 1
+    }
+    response = requests.get(READ_API_BASE_URL, params=params)
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        feeds = data.get('feeds', [])
+        if feeds:
+            last_feed = feeds[0]
+            temperature = float(last_feed.get('field1', 0))
+            return temperature
+    return None
+
+
+def set_state(on):
+    value = "1" if on else "0"
+    params = {
+        "api_key": WRITE_API_KEY,
+        "field1": value
+    }
+    response = requests.get(WRITE_API_BASE_URL, params=params)
+    print(f"Response Text: {response.text}")  # Print response text for debugging
+    if response.status_code == 200:
+        print(f"Successfully set state to {'On' if on else 'Off'}.")
+    else:
+        print(f"Failed to set state. Status code: {response.status_code}")
+
+
+# Get the last temperature reading
+temperature = get_last_temperature()
+
+if temperature is not None:
+    print(f"Last temperature reading: {temperature}°C")
+
+    # Analyze the temperature and set the state
+    if temperature > TEMPERATURE_THRESHOLD:
+        print("Temperature exceeds threshold. Turning On.")
+        set_state(True)
+    else:
+        print("Temperature is below threshold. Turning Off.")
+        set_state(False)
+else:
+    print("Failed to retrieve temperature data.")
+
+# For the PDLC Glass:
+# pdlc_tint = 1 if uv_light < 90 else 0            # PDLC Glass Tinting
