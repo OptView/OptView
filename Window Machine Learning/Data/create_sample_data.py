98cae583dcc3f7ee41bc37602dfcdf54311b1c6f
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-10-27 17:25:25 -0400

    Update "creat sample data" file

--- Window Machine Learning/Data/create_sample_data.py
+++ Window Machine Learning/Data/create_sample_data.py
@@ -3,60 +3,44 @@ import csv
 
 
 data_samples = []
+data_count = 10000000
 
-# Comfortable Day (20 samples)
-for _ in range(20):
+# Comfortable Day
+for _ in range(data_count):
     data_samples.append([round(random.uniform(20.0, 25.0), 2),  # Temperature
                          round(random.uniform(45.0, 60.0), 2),  # Humidity
-                         round(random.uniform(5.0, 10.0), 2),   # UV Light
                          random.randint(20, 50),                 # IAQ Index
-                         0,                                      # Fire Sensor
                          1])                                     # Window
 
-# Hot Day (20 samples)
-for _ in range(20):
-    data_samples.append([round(random.uniform(28.0, 35.0), 2),  # Temperature
-                         round(random.uniform(65.0, 85.0), 2),  # Humidity
-                         round(random.uniform(15.0, 20.0), 2),  # UV Light
+# Hot Day
+for _ in range(data_count):
+    data_samples.append([round(random.uniform(30.0, 60.0), 2),  # Temperature
+                         round(random.uniform(50.0, 85.0), 2),  # Humidity
                          random.randint(50, 100),                # IAQ Index
-                         0,                                      # Fire Sensor
                          0])                                     # Window
 
-# Polluted Day (20 samples)
-for _ in range(20):
+# Polluted Day
+for _ in range(data_count):
     data_samples.append([round(random.uniform(20.0, 28.0), 2),  # Temperature
                          round(random.uniform(50.0, 70.0), 2),  # Humidity
-                         round(random.uniform(10.0, 15.0), 2),  # UV Light
-                         random.randint(100, 150),               # IAQ Index
-                         0,                                      # Fire Sensor
+                         random.randint(100, 500),               # IAQ Index
                          0])                                     # Window
 
-# Fire Alert (20 samples)
+# High Humidity Day
 for _ in range(20):
     data_samples.append([round(random.uniform(20.0, 30.0), 2),  # Temperature
-                         round(random.uniform(50.0, 70.0), 2),  # Humidity
-                         round(random.uniform(10.0, 15.0), 2),  # UV Light
-                         random.randint(200, 250),               # IAQ Index
-                         1,                                      # Fire Sensor
+                         round(random.uniform(80.0, 95.0), 2),  # Humidity
+                         random.randint(50, 100),                # IAQ Index
                          1])                                     # Window
 
-# Cold Day (20 samples)
-for _ in range(20):
-    data_samples.append([round(random.uniform(15.0, 20.0), 2),  # Temperature
+
+# Cold Day (10000 samples)
+for _ in range(data_count):
+    data_samples.append([round(random.uniform(-50.0, 0.0), 2),  # Temperature
                          round(random.uniform(30.0, 45.0), 2),  # Humidity
-                         round(random.uniform(5.0, 10.0), 2),   # UV Light
                          random.randint(20, 50),                 # IAQ Index
-                         0,                                      # Fire Sensor
                          1])                                     # Window
 
-# Industrial Activity Nearby (20 samples)
-for _ in range(20):
-    data_samples.append([round(random.uniform(20.0, 28.0), 2),  # Temperature
-                         round(random.uniform(50.0, 70.0), 2),  # Humidity
-                         round(random.uniform(10.0, 15.0), 2),  # UV Light
-                         random.randint(100, 150),               # IAQ Index
-                         0,                                      # Fire Sensor
-                         0])                                     # Window
 
 # Print the generated samples
 for sample in data_samples:
@@ -67,7 +51,7 @@ for sample in data_samples:
 with open("data_samples.csv", "w", newline="") as csvfile:
     csvwriter = csv.writer(csvfile)
     # Writing the header
-    csvwriter.writerow(["Temperature", "Humidity", "UV Light", "IAQ Index", "Fire Sensor", "Window"])
+    csvwriter.writerow(["Temperature", "Humidity", "IAQ Index", "Window"])
     # Writing the data samples
     csvwriter.writerows(data_samples)
 
