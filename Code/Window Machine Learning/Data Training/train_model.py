ad54d7b862a1a72c5898e04c220372a9af8c365d
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-11-02 11:45:32 -0400

    Added more scores to ensure the accuracy of the model trained

--- Code/Window Machine Learning/Data Training/train_model.py
+++ Code/Window Machine Learning/Data Training/train_model.py
@@ -1,7 +1,8 @@
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score  # Added metrics for evaluation
 import pandas as pd
-import joblib  # for saving and loading the model
+import joblib
 
 # Load data from CSV
 data = pd.read_csv('../Data/data_samples.csv')
@@ -10,8 +11,9 @@ data = pd.read_csv('../Data/data_samples.csv')
 X = data.drop(columns=['Window'])
 y = data['Window']
 
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Split data into training, validation, and testing sets
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
 
 # Train a decision tree classifier
 clf = DecisionTreeClassifier(random_state=42)
@@ -20,6 +22,24 @@ clf.fit(X_train, y_train)
 # Save the trained model
 joblib.dump(clf, 'trained_decision_tree_model.pkl')
 
-# Evaluate the model
-accuracy = clf.score(X_test, y_test)
+# Predict on test set
+y_pred = clf.predict(X_test)
+
+# Evaluate the model using various metrics
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
 print(f"Model Accuracy: {accuracy*100:.2f}%")
+print(f"Precision: {precision*100:.2f}%")
+print(f"Recall: {recall*100:.2f}%")
+print(f"F1-Score: {f1*100:.2f}%")
+
+# Predict on the validation set
+y_val_pred = clf.predict(X_val)
+
+# Evaluate the model's performance on the validation set using accuracy
+val_accuracy = accuracy_score(y_val, y_val_pred)
+print(f"Validation Accuracy: {val_accuracy*100:.2f}%")
+
