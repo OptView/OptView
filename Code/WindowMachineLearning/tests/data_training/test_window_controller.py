4d316eea49bd1b836380125c245bd34de82bac1d
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2023-11-22 20:53:26 -0500

    Completed testing the window controller

--- Code/WindowMachineLearning/tests/data_training/test_window_controller.py
+++ Code/WindowMachineLearning/tests/data_training/test_window_controller.py
@@ -5,7 +5,7 @@ class TestWindowController(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # Set up the path to the model file for testing
+        """Set up the WindowController with a model path for testing."""
 
         # For testing directory
         # model_path = '../models/trained_decision_tree_model.pkl'
@@ -19,26 +19,41 @@ class TestWindowController(unittest.TestCase):
         # Instantiate the WindowController with the model path
         cls.window_controller = WindowController(cls.model_path)
 
-    def test_normal(self):
-        # Normal conditions - expecting the window to be opened
-        data = [20.00, 35.81, 50.0]  # Temperature, Humidity, IAQ Index
+    def test_comfortable_day_open_window(self):
+        """Test that the window opens on a comfortable day."""
+        data = [20.0, 35.0, 50.0]  # Temperature, Humidity, IAQ Index
         action = self.window_controller.make_prediction(data)
         expected_output = 1  # 1 indicates open the window
         self.assertEqual(action, expected_output)
 
-    def test_max_normal(self):
-        # Edge of normal conditions - expecting the window to be opened
-        data = [30.00, 60.00, 100.0]  # Temperature, Humidity, IAQ Index
+    def test_hot_day_close_window(self):
+        """Test that the window closes on a hot day."""
+        data = [40.0, 60.0, 50.0]
         action = self.window_controller.make_prediction(data)
-        expected_output = 1  # 1 indicates open the window
+        expected_output = 0  # 0 indicates close the window
         self.assertEqual(action, expected_output)
 
-    def test_min_normal(self):
-        # Minimum normal conditions - expecting the window to be opened
-        data = [10.00, 00.00, 00.0]  # Temperature, Humidity, IAQ Index
+    def test_polluted_day_close_window(self):
+        """Test that the window closes on a polluted day."""
+        data = [25.0, 50.0, 200.0]
         action = self.window_controller.make_prediction(data)
-        expected_output = 1  # 1 indicates open the window
+        expected_output = 0  # 0 indicates close the window
         self.assertEqual(action, expected_output)
+
+    def test_high_humidity_day_close_window(self):
+        """Test that the window closes on a high humidity day."""
+        data = [30.0, 85.0, 50.0]
+        action = self.window_controller.make_prediction(data)
+        expected_output = 0  # 0 indicates close the window
+        self.assertEqual(action, expected_output)
+
+    def test_cold_day_close_window(self):
+        """Test that the window closes on a cold day."""
+        data = [-10.0, 50.0, 50.0]
+        action = self.window_controller.make_prediction(data)
+        expected_output = 0  # 0 indicates close the window
+        self.assertEqual(action, expected_output)
+
 
 # The following line would run the tests if uncommented and this file is executed
 # if __name__ == '__main__':
